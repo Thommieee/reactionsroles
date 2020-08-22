9d6f697e7285f6caf2f96f0144b982b95aef5f4b
Thommieee <60047417+Thommieee@users.noreply.github.com>
2020-08-22 19:19:40 +0200

    Added a new command
    Open en close NS

---
+++ commands/closechannel.js
@@ -0,0 +1,21 @@
+const discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+
+    const role =  message.guild.roles.cache.find("731837368281202688");
+
+    var channel1 = message.guild.channels.cache.get("746661088912867360")
+
+    var channel2 = message.guild.channels.cache.get("746661126367870986")
+
+    channel1.overwritePermissions(role, { 'VIEW_CHANNEL': false })
+
+    channel2.overwritePermissions(role, { 'VIEW_CHANNEL': false })
+
+}
+
+module.exports.help = {
+    name: "closeNS",
+    description: "Als je dit command gebruikt zegt de bot hallo terug",
+    category: "Algemeen"
+}
